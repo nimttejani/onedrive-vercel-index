2775f077b86b48988140840adccccb1c03bbe9fb
Neet Tejani <134366616+nimttejani@users.noreply.github.com>
2023-11-03 20:28:26 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,10 +17,10 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Vinfall's Sage Stone",
+  title: "Cloudz",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Public',
+  baseDirectory: '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -38,15 +38,11 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/Vinfall/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a> with ❤ by Vinfall.',
+    'Made with ❤ by Imcloud.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: [
-    '/㊙️ Private',
-    '/🎵 Music',
-    '/🥁 Taiko/🎶 TJA/🎵 Real TJA'
-  ],
+  protectedRoutes: [],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
@@ -54,16 +50,7 @@ module.exports = {
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [
-    {
-      name: 'Blog',
-      link: 'https://blog.vinfall.com',
-    },
-    {
-      name: 'GitHub',
-      link: 'https://github.com/Vinfall',
-    },
-  ],
+  links: [],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
